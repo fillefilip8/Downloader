57b14cef18c78c6f010271e20efffd49adf27acf
fillefilip8 <contact@fillefilip8.xyz>
2022-07-29 13:56:55 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["Downloader.csproj", "Downloader.csproj"]
+RUN dotnet restore "Downloader.csproj"
+COPY . .
+RUN dotnet build "Downloader.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Downloader.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Downloader.dll"]
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CommandLine;
+using System.IO.Compression;
+
+var urlArg = new Argument<string>
+    ("url", "URL");
+
+var destinationArg = new Argument<string>
+    ("destination", "Destination");
+
+var extractOption = new Option<bool>("--extract", "Extract");
+
+var rootCommand = new RootCommand("Downloader")
+{
+    urlArg,
+    destinationArg,
+    extractOption
+};
+rootCommand.SetHandler(async (url, destination, extract) =>
+{
+    if (string.IsNullOrEmpty(url))
+    {
+        Console.WriteLine("No url provided");
+        return;
+    }
+                
+    if (string.IsNullOrEmpty(destination))
+    {
+        Console.WriteLine("No destination provided");
+        return;
+    }
+
+    var httpClient = new HttpClient();
+    
+    var stream = await httpClient.GetStreamAsync(url);
+
+    var newDest = extract ? Path.GetTempFileName() : destination;
+
+    Console.WriteLine($"Downloading {url} to {newDest}");
+    await using (var fileStream = File.OpenWrite(newDest))
+    {
+        await stream.CopyToAsync(fileStream);
+    }
+    Console.WriteLine($"Downloaded {url} to {newDest}");
+
+                
+    if (extract)
+    {
+        Console.WriteLine($"Extracting {newDest} to {destination}");
+        ZipFile.ExtractToDirectory(newDest, destination, true);
+        Console.WriteLine($"Extracted {newDest} to {destination}");
+    }
+}, urlArg, destinationArg, extractOption);
+
+
+await rootCommand.InvokeAsync(args);
